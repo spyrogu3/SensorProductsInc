9e962546927f848a86be15c997ba45aefae5e258
spyrogu3 <104386337+spyrogu3@users.noreply.github.com>
2022-04-25 14:16:06 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,75 @@
+import './App.css';
+
+
+var colorTable = [
+  [3,0,0,130],
+  [6,0,0,153],
+  [9,0,0,181],
+  [12,0,0,202],
+  [15,0,0,224],
+  [18,0,32,241],
+  [21,0,66,254],
+  [24,0,95,255],
+  [27,0,128,255],
+  [30,0,144,255],
+  [33,0,160,255],
+  [36,0,175,255],
+  [39,0,193,255],
+  [42,0,207,255],
+  [45,0,226,255],
+  [48,0,240,223],
+  [51,0,255,194],
+  [54,65,255,128],
+  [57,129,255,64],
+  [60,162,255,33],
+  [63,194,255,0],
+  [66,223,255,0],
+  [69,255,255,0],
+  [72,255,239,0],
+  [75,255,223,0],
+  [78,255,200,0],
+  [81,255,175,0],
+  [84,255,160,0],
+  [87,255,145,0],
+  [90,255,121,0],
+  [93,255,96,0],
+  [96,255,48,0],
+  [100,255,0,0]];
+
+  let i = 0;
+
+function Isobar(i,colorTable){
+  let gradient = "";
+  for (let i = 0; i < colorTable.length-1; i++){
+    if(colorTable[i] == 0){
+      colorTable[i] += 1;
+    }
+    
+    if(colorTable[i] == colorTable.length){
+      gradient += "rgba("+colorTable[i] +')';
+    }else{
+      gradient += "rgba("+colorTable[i] +'),';
+    }
+  }
+  
+  gradient += "rgba("+colorTable[colorTable.length-1] +')'
+
+  return gradient
+  
+}
+
+const App = () => {
+  return (
+    
+    <body style={{ marginLeft: '4%', marginTop: '1%', width: '3%',height:'800px', border:'2px solid black' ,background:'linear-gradient('+Isobar(i,colorTable)+')'}}>
+      <table className='table'>
+        <tr className='high'>High</tr>
+        <tr className='pressure'>Pressure</tr>
+        <tr className='low'>Low</tr>
+      </table>
+    </body>
+    
+  );
+}
+
+export default App;
---
+++ Title.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Title(){
+    return <h2>My first component</h2>
+}
+
+export default Title;
